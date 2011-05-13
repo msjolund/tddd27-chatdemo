8c0b6f7110e65231833654d2cb10d98b32bfc839
Micael Sjolund <micael.sjolund@esn.me>
2011-05-13 11:49:25 +0200

    Time format fix. Styling.

--- src/tddd27/surface/components/main/main_controller.py
+++ src/tddd27/surface/components/main/main_controller.py
@@ -6,12 +6,12 @@ class MainController(ApplicationController):
     def index(self):
         self.pushAddChannel("**global_chat")
         return self.render('main.index')
-
+    
     def msg_submit(self):
         body = self.post.get("body", "-")
         username = self.post.get("username", "John Doe")
-        timestamp = datetime.now().strftime("%H %M")
-        message = { "body": body, "username": username, "timestamp": timestamp}
+        timestamp = datetime.now().strftime("%H:%M")
 
+        message = { "body": body, "username": username, "timestamp": timestamp}
         Push.sendToChannel("**global_chat", "ChatMsgReceived", message)
         return SurfaceResponse("ok")
