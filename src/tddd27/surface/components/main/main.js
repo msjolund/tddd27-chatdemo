989b2f8326440868a880e403306c0f09b0496694
ESN <ESN@ESNs-MacBook-Air.local>
2011-05-18 13:14:48 +0200

    Readded cut out functionality.

--- src/tddd27/surface/components/main/main.js
+++ src/tddd27/surface/components/main/main.js
@@ -4,11 +4,18 @@ var main = {
 
         $("form").submit(function (e) {
             e.preventDefault();
-
+            $.post($(this).attr("action"), $(this).serialize());
+            $("input[name=body]").val("").focus();
         });
 
         $("input[type=text]").placeholder();
 
 
+        Push.bind("ChatMsgReceived", function (event) {
+            var msgElement = $("#chatMsgTmpl").tmpl(event.data);
+            $(".chat ul").append(msgElement)
+            $(".chat .scroller").scrollTop($(".chat .scroller ul").outerHeight())
+
+        })
     }
 };
